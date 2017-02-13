580d8de82ed3a7eeeb3b7b6373757ace2856aca6
Markus Kappel <markus.kappel@gmail.com>
2017-02-13 23:11:42 +0100

    Adds spring cloud config composite repo example.

---
+++ src/main/java/com/sample/ConfigServer.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
